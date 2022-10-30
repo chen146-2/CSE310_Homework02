e2f8c2327e87241a1e6fa98cb3e9e830349c33bb
Kevin Chen <kevin.chen.15@stonybrook.edu>
2022-10-30 03:23:40 -0400

    Changes to the file, boolean to ensure packets sent after <FIN> flag are not processed in the throughput calculation

--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -3,12 +3,24 @@ import dpkt
 
 f = open('/Users/kevin/Downloads/assignment2.pcap','rb')
 pcap = dpkt.pcap.Reader(f)
+f2 = open('/Users/kevin/Downloads/assignment2.pcap','rb')
+pcap2 = dpkt.pcap.Reader(f2)
 count = 0
 flow = {}
 times={}
 sender='130.245.145.12'
 receiver='128.208.2.198'
 receiver_port = 80
+for ts, buf in pcap2:
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    if tcp.flags==2:
+        times[tcp.sport]= []
+        times[tcp.sport].append(ts)
+    elif tcp.flags==17 and tcp.sport == receiver_port:
+        times[tcp.dport].append(ts)
+f2.close()
 for ts, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
     ip = eth.data
@@ -16,31 +28,35 @@ for ts, buf in pcap:
 
     if tcp.flags==2:
         flow[tcp.sport] = {}
-        times[tcp.sport] = []
-        times[tcp.sport].append(ts)
-        flow[tcp.sport]['throughput']=len(tcp)
-    elif tcp.flags==17 and tcp.sport == receiver_port:
-        times[tcp.dport].append(ts)
+        #times[tcp.sport] = []
+        #times[tcp.sport].append(ts)
+        flow[tcp.sport]['throughput']=0
     elif tcp.flags==16 and tcp.sport != receiver_port:
         if (len(flow[tcp.sport])==1):
             flow[tcp.sport]['win']=tcp.win
             flow[tcp.sport][tcp.seq]=tcp.ack
         elif(len(flow[tcp.sport])<5):    
             flow[tcp.sport][tcp.seq]=tcp.ack
-        flow[tcp.sport]['throughput'] += len(tcp)
+            if len(tcp.data)>0:
+                flow[tcp.sport]['throughput'] +=len(tcp)
+        elif ts < times[tcp.sport][1] and len(tcp.data)>0:
+            flow[tcp.sport]['throughput'] += len(tcp)
     elif tcp.flags !=16 and tcp.sport!= receiver_port:
-        flow[tcp.sport]['throughput']+=len(tcp)
+        if ts < times[tcp.sport][1] and len(tcp.data)>0:
+            flow[tcp.sport]['throughput']+=len(tcp)
     count+=1
 
 # this part is to calculate the throughputs for each flow
 
 throughputs=[]
-
+print('----------- times -------------')
 for value in times:
+    print(times[value][1]-times[value][0])
     throughputs.append(times[value][1]-times[value][0])
 count=0
-
+print('-------- throughput -----------')
 for value in flow:
+    print(flow[value]['throughput'])
     throughputs[count]=(flow[value]['throughput']/throughputs[count])
     count+=1
 
@@ -74,7 +90,7 @@ for value in flow:
     print(f'''
                 FIRST TWO TRANSACTIONS AFTER CONNECTION SETUP FOR FLOW {index}
     ------------------------------------------------------------------------------------------
-    |          SOURCE  ==>  DESTINATION         |   (SEQ NUMBER, ACK NUMBER)  | WINDOW SIZE |
+    |          SOURCE  ==>  DESTINATION         |   (SEQ NUMBER, ACK NUMBER)  | WINDOW SIZE  |
     ------------------------------------------------------------------------------------------
     | {sender}:{value} ==> {receiver}:{receiver_port} | ( {seqNum01} , {ackNum01} ) |      {window}       |
     | {receiver}:{receiver_port} ==> {sender}:{value} | ( {ackNum01} , {seqNum02} ) |      {window}       |
